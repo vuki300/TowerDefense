e0c86612a5a6ce3398949cbee99dbbd2a7e6650f
wolfvuki064 <vuk3003@Gmail.com>
2018-05-16 09:18:35 +0200

    Tower Defense
    123

---
+++ Assets/Scripts/EnemyWalk.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class EnemyWalk : MonoBehaviour
+{
+    public float speed = 10f;
+    public float turning = 3f;
+    private Transform target;
+    private int wavepointIndex = 0;
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= turning)
+        {
+            GetNextWaypoint();
+        }
+    }
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+}
